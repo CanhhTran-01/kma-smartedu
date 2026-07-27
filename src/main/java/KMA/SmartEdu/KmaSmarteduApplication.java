3f7957cbcc9439942da2a1165c664e206cda592e
Tran Duc Canh <levitran1102005@gmail.com>
2026-07-27 15:59:57 +0700

    Initial commit

---
+++ src/main/java/KMA/SmartEdu/KmaSmarteduApplication.java
@@ -0,0 +1,13 @@
+package KMA.SmartEdu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmaSmarteduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmaSmarteduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/KMA/SmartEdu/KmaSmarteduApplicationTests.java
@@ -0,0 +1,13 @@
+package KMA.SmartEdu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmaSmarteduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
